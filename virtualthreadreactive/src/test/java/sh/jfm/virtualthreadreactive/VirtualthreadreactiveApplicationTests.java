abc4f333ddc6831977395257884aaecda150cbfb
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-09-16 10:34:26 -0500

    Spring Init

---
+++ virtualthreadreactive/src/test/java/sh/jfm/virtualthreadreactive/VirtualthreadreactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.jfm.virtualthreadreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadreactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
